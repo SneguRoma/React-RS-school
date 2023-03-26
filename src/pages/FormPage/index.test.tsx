d4910329d9fa88200e4f96b37db636c1e964bea1
sneguroma <sneguroma@gmail.com>
2023-03-26 15:22:17 +0200

    feat: tests added

--- src/pages/FormPage/index.test.tsx
+++ src/pages/FormPage/index.test.tsx
@@ -8,16 +8,15 @@ describe('FormPage', () => {
     render(<FormPage className="test" />);
     expect(screen.getByPlaceholderText('input name')).toBeInTheDocument();
   });
-  it('render name of cards text', () => {
+  it('render fields of card', () => {
     render(<FormPage className="test" />);
-    /* expect(screen.getByText('Name: Terry Medhurst')).toBeInTheDocument();
-    expect(screen.getByText('phone: +63 791 675 8914')).toBeInTheDocument();
-    expect(screen.getByText('gender: male age:50')).toBeInTheDocument(); */
+    expect(screen.getByText('Select colors of animal:')).toBeInTheDocument();
+    expect(screen.getByText('Select gender:')).toBeInTheDocument();
+    expect(screen.getByText('Date of birth:')).toBeInTheDocument();
   });
-  it('render name of card text', () => {
+  it('render button and heading', () => {
     render(<FormPage className="test" />);
-    //expect(screen.getAllByRole('input')).toHaveLength(9);
     expect(screen.getByRole('button')).toBeInTheDocument();
-    //expect(screen.getAllByRole('heading', { level: 2 })).toHaveLength(20);
+    expect(screen.getAllByRole('heading', { level: 4 })).toHaveLength(6);
   });
 });
